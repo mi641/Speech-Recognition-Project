5fb31c20f660591f714aefb47975f7ca8b2bf5ac
mi641 <57021072+mi641@users.noreply.github.com>
2020-01-16 13:40:24 +0000

    basic sound to text code

---
+++ speech.py
@@ -0,0 +1,8 @@
+import speech_recognition as sr
+##r = sr.Microphone as Source:
+print("say a word:")
+audio = r.listen(source)
+try: print("you said:" r.recognize(audio))
+ErrExcept ("your voice low")
+except 
+print("not audible" {0} }
